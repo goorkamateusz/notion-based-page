bad9412049ef2e19c258a9032a14d820b972623a
Górka Mateusz <maatiug@gmail.com>
2024-02-11 00:42:13 +0100

    [wip]

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
+  output: "standalone",
   reactStrictMode: true,
   staticPageGenerationTimeout: 300,
   images: {
